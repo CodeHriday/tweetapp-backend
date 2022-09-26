e1029001eb4526e6647bbbb6a610789b1c777f6b
CodeHriday <74758879+CodeHriday@users.noreply.github.com>
2022-09-26 17:55:59 +0530

    Update TweetAppApplication.java

--- src/main/java/com/hriday/rao/TweetAppApplication.java
+++ src/main/java/com/hriday/rao/TweetAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-public class TweetAppApplication {
+public class TweetAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TweetAppApplication.class, args);
