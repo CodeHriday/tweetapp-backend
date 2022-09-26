3f1d019362bfa9b226074556a992eb7da3820af8
CodeHriday <74758879+CodeHriday@users.noreply.github.com>
2022-09-26 17:57:39 +0530

    Update TweetAppApplication.java

--- src/main/java/com/hriday/rao/TweetAppApplication.java
+++ src/main/java/com/hriday/rao/TweetAppApplication.java
@@ -2,6 +2,7 @@ package com.hriday.rao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
